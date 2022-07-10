a92de19d5f181ce140877445b3008d703cce9dcf
Ian-Marco-Moffett <spurious.vector@gmail.com>
2022-07-10 01:43:34 -0400

    feat: PCI access.

---
+++ kernel/include/arch/bus/pci/def.h
@@ -0,0 +1,104 @@
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2022 N3xusProject, Ian Marco Moffett
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#ifndef PCI_DEFINITIONS_H
+#define PCI_DEFINITIONS_H
+
+
+typedef enum 
+{
+    MASS_STORAGE_CONTROLLER = 0x1,
+    NETWORK_CONTROLLER = 0x2,
+    DISPLAY_CONTROLLER = 0x3,
+    MULTIMEDIA_CONTROLLER = 0x4,
+    MEMORY_CONTROLLER = 0x5,
+    BRIDGE = 0x6,
+} PCI_CLASSCODE;
+
+
+typedef enum 
+{
+    SCSI_BUS_CONTROLLER = 0x0,
+    IDE_CONTROLLER = 0x1,
+    FLOPPY_DISK_CONTROLLER = 0x2,
+    IPI_BUS_CONTROLLER = 0x3,
+    RAID_CONTROLLER = 0x4,
+    ATA_CONTROLLER = 0x5,
+    SERIAL_ATA_CONTROLLER = 0x5,
+    SERIAL_ATTACHED_SCSI_CONTROLLER = 0x7,
+    NON_VOLATILE_MEMORY_CONTROLLER = 0x8
+} MASS_STORAGE_SUBCLASS;
+
+
+typedef enum 
+{
+    ETHERNET_CONTROLLER = 0x0,
+    TOKEN_RING_CONTROLLER = 0x1,
+    FDDI_CONTROLLER = 0x2,
+    ATM_CONTROLLER = 0x3,
+    ISDN_CONTROLLER = 0x4,
+    WORD_FIP_CONTROLLER = 0x5,
+    PICMG_MULTI_COMPUTING_CONTROLLER = 0x6,
+    INFINBAND_CONTROLLER = 0x7,
+    FABRIC_CONTROLLER = 0x8
+} NETWORK_CONTROLLER_SUBCLASS;
+
+
+typedef enum
+{
+    VGA_COMPATIBLE_CONTROLLER = 0x0,
+    XGA_CONTROLLER = 0x1,
+    _3D_CONTROLLER = 0x2,
+} DISPLAY_CONTROLLER_SUBCLASS;
+
+
+typedef enum 
+{
+    MULTIMEDIA_VIDEO_CONTROLLER = 0x0,
+    MULTIMEDIA_AUDIO_CONTROLLER = 0x1,
+    COMPUTER_TELEPHONY_DEVICE = 0x2,
+    AUDIO_DEVICE = 0x3
+} MULTIMEDIA_CONTROLLER_SUBCLASS;
+
+typedef enum
+{
+    RAM_CONTROLLER = 0x0,
+    FLASH_CONTROLLER = 0x1
+} MEMORY_CONTROLLER_SUBCLASS;
+
+
+typedef enum 
+{
+    HOST_BRIDGE = 0x0,
+    ISA_BRIDGE = 0x1,
+    EISA_BRIDGE = 0x2,
+    MCA_BRIDGE = 0x3,
+    PCI_TO_PCI_BRIDGE = 0x4,
+    PCMICIA_BRIDGE = 0x5,
+    NUBUS_BRIDGE = 0x6,
+    CARDBUS_BRIDGE = 0x7,
+    RACEWAY_BRIDGE = 0x8,
+} BRIDGE_SUBCLASS;
+
+#endif
---
+++ kernel/src/arch/bus/pci/access.c
@@ -0,0 +1,139 @@
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2022 N3xusProject, Ian Marco Moffett
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#include <arch/bus/pci/access.h>
+#include <arch/bus/io/io.h>
+
+#define CONFIG_ADDRESS 0xCF8
+#define CONFIG_DATA 0xCFC
+
+/*
+ *  @brief  Reads a word from a device's configuration
+ *          space. A configuration space is a set of registers
+ *          that are memory mapped. This registers are used for
+ *          configuration.
+ *
+ *
+ * @param bus       Bus we want to select.
+ * @param slot      Device number we want to select.
+ * @param func      A device function we could select.
+ * @param offset    Register offset.
+ *
+ */
+
+static uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset) 
+{
+    uint32_t lbus = (uint32_t)bus;
+    uint32_t lslot = (uint32_t)slot;
+    uint32_t lfunc = (uint32_t)func;
+    uint16_t tmp = 0;
+
+    uint32_t config_address = (uint32_t)((lbus << 16) | (lslot << 11) |
+              (lfunc << 8) | (offset & 0xFC) | ((uint32_t)0x80000000));
+
+    // Write out the address that is required to be
+    // accessed.
+    outportl(CONFIG_ADDRESS, config_address);
+
+    // Read in configuration-space data.
+    tmp = (uint16_t)((inportl(0xCFC) >> ((offset & 2) * 8)) & 0xFFFF);
+    return tmp;
+}
+
+
+uint16_t pci_read_vendor_id(uint8_t bus, uint8_t slot)
+{
+    return pci_config_read_word(bus, slot, 0, 0);
+}
+
+
+uint16_t pci_read_device_id(uint8_t bus, uint8_t slot)
+{
+    return pci_config_read_word(bus, slot, 0, 2);
+}
+
+
+uint8_t pci_read_class_code(uint8_t bus, uint8_t slot)
+{
+    return pci_config_read_word(bus, slot, 0, 0xA) >> 8;
+}
+
+
+uint8_t pci_read_subclass_code(uint8_t bus, uint8_t slot)
+{
+    return pci_config_read_word(bus, slot, 0, 0xA) & 0xFF;
+}
+
+
+uint32_t pci_get_bar0(uint8_t bus, uint8_t slot)
+{
+    uint16_t low_word = pci_config_read_word(bus, slot, 0, 0x10);
+    uint16_t high_word = pci_config_read_word(bus, slot, 0, 0x12);
+
+    return (uint32_t)((low_word << 16) | high_word);
+}
+
+
+uint32_t pci_get_bar1(uint8_t bus, uint8_t slot)
+{
+    uint16_t low_word = pci_config_read_word(bus, slot, 0, 0x14);
+    uint16_t high_word = pci_config_read_word(bus, slot, 0, 0x16);
+
+    return (uint32_t)((low_word << 16) | high_word);
+}
+
+
+uint32_t pci_get_bar2(uint8_t bus, uint8_t slot)
+{
+    uint16_t low_word = pci_config_read_word(bus, slot, 0, 0x18);
+    uint16_t high_word = pci_config_read_word(bus, slot, 0, 0x1A);
+
+    return (uint32_t)((low_word << 16) | high_word);
+}
+
+
+uint32_t pci_get_bar3(uint8_t bus, uint8_t slot)
+{
+    uint16_t low_word = pci_config_read_word(bus, slot, 0, 0x1C);
+    uint16_t high_word = pci_config_read_word(bus, slot, 0, 0x1E);
+
+    return (uint32_t)((low_word << 16) | high_word);
+}
+
+uint32_t pci_get_bar4(uint8_t bus, uint8_t slot)
+{
+    uint16_t low_word = pci_config_read_word(bus, slot, 0, 0x20);
+    uint16_t high_word = pci_config_read_word(bus, slot, 0, 0x22);
+
+    return (uint32_t)((low_word << 16) | high_word);
+}
+
+
+uint32_t pci_get_bar5(uint8_t bus, uint8_t slot)
+{
+    uint16_t low_word = pci_config_read_word(bus, slot, 0, 0x24);
+    uint16_t high_word = pci_config_read_word(bus, slot, 0, 0x26);
+
+    return (uint32_t)((low_word << 16) | high_word);
+}
