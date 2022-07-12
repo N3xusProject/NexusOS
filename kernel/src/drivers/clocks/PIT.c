7b2e9666cff09eb91cd68a02507302353abdec2a
Ian-Marco-Moffett <spurious.vector@gmail.com>
2022-07-12 01:44:10 -0400

    feat: PIT driver.

---
+++ kernel/src/drivers/clocks/PIT.c
@@ -0,0 +1,34 @@
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2022 N3xusProject, Ian Marco Moffett
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#include <drivers/clocks/PIT.h>
+#include <arch/bus/io/io.h>
+
+void pit_set_phase(uint16_t hz)
+{
+    int divisor = 1193180 / hz;         // Calculate divisor.
+    outportb(0x43, 0x36);               // Command byte.
+    outportb(0x40, divisor & 0xFF);     // Low byte of divisor.
+    outportb(0x40, divisor >> 8);       // High byte of divisor.
+}
