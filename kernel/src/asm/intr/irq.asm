cfca7cd2c2aa5aad1e7332621ca6a78fcf70219c
Ian-Marco-Moffett <spurious.vector@gmail.com>
2022-07-12 01:44:20 -0400

    feat: IRQs.

---
+++ kernel/src/asm/intr/irq.asm
@@ -0,0 +1,58 @@
+;; Copyright (c) 2022 N3xusProject, Ian Marco Moffett, All Rights Reserved.
+;; See LICENSE file.
+
+bits 64
+global irq0
+
+extern lapic_send_eoi
+extern kprintf
+
+%macro _pushall 0
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+    push rbp
+    push rsp
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+%endmacro
+
+%macro _popall 0
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rsp
+    pop rbp
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+%endmacro
+
+irq0:
+    cli
+    _pushall
+    inc word [pit_ticks]
+    call lapic_send_eoi
+    _popall
+    sti
+    iretq
+
+section .data
+pit_ticks: dw 0
