5a3d420633c0bfae1f0548b3f037ae76b579002d
Ian <spurious.vector@gmail.com>
2022-07-24 03:24:25 -0400

    feat: draw square.

--- system/nexd/src/main.c
+++ system/nexd/src/main.c
@@ -2,10 +2,30 @@
 
 int main(void)
 {
-    __asm__ __volatile__("mov $0, %rax; int $0x80");
+    __asm__ __volatile__("mov $0, %rax; int $0x80"); 
+    
+    for (uint32_t x = 50; x < 150; ++x)
+    {
+        for (uint32_t y  = 50; y < 150; ++y)
+        {
+            __asm__ __volatile__(
+                "\
+                mov %0, %%r11;     /* X */ \
+                mov %1, %%r10;     /* Y */ \
+                mov $0x1, %%rax; \
+                mov $0x1, %%rbx; \
+                mov $0x20309d850a0, %%rcx; \
+                mov %%r11, %%rdx; \
+                mov %%r10, %%rsi; \
+                mov $0x0000FF, %%rdi; \
+                int $0x80" :: "m" (x), "m" (y));
+        }
+    }
 
     while (1)
     {
+
+
         __asm__ __volatile__(
                 "\
                 mov $0x2, %rax; /* devctl_in */ \
