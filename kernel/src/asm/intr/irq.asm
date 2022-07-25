f87308ef8afc26c402e015c09253bd759dea0dc3
Ian <spurious.vector@gmail.com>
2022-07-24 22:37:22 -0400

    refactor: removed junk.

--- kernel/src/asm/intr/irq.asm
+++ kernel/src/asm/intr/irq.asm
@@ -9,44 +9,6 @@ extern lapic_send_eoi
 extern kprintf
 extern switch_thread
 
-%macro _pushall 0
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rsi
-    push rdi
-    push rbp
-    push rsp
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
-    push r15
-%endmacro
-
-%macro _popall 0
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rsp
-    pop rbp
-    pop rdi
-    pop rsi
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
-%endmacro
-
 irq0:
     cli
     inc word [pit_ticks]
